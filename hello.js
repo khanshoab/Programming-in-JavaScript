166505910a65b5214e364f5c180fed8dc79a5693
khan shoab <khanshoab19co33@gmail.com>
2021-09-23 16:11:00 +0530

    In this tutorial we learn core JavaScript.

---
+++ hello.js
@@ -0,0 +1,5 @@
+
+
+
+console.log("MY NAME IS KHAN SHOAB AKHTAR VAKIL AHMAD");
+console.log(" HELLO I AM GOOGLE HOW CAN I HELP");
---
+++ my.js
@@ -0,0 +1,6 @@
+var i = 0
+for(; i<5; i++)
+{
+    document.write(i + "<br>");
+}
+document.write("<br> rest of the code is here")
